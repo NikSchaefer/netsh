03b03545636423177a0f415c7353ef224a2fbdab
NikSchaefer <nikkschaefer@gmail.com>
2021-03-30 13:27:13 -0500

    Create Profile Files

--- main.go
+++ main.go
@@ -2,28 +2,51 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
 	"os/exec"
+	"strings"
 )
 
 func main() {
 
-	// name := "sideswipe"
-	// password := ""
+	name := "sideswipe"
+	password := "myPassword"
 
-	// passed := loginToNetwork(name, password)
-	// fmt.Println(passed)
+	createFileProfile(name, password)
 
-	addProfile("J")
 }
 
-func loginToNetwork(name, password string) bool {
+func createFileProfile(name, pass string) error {
+	filename := fmt.Sprintf("_%s.xml", name)
+	file, err := os.Create(filename)
+	must(err)
+
+	data, err := ioutil.ReadFile("profile.xml")
+	must(err)
+
+	str := string(data)
+	str = strings.Replace(str, "{password}", pass, 1)
+	str = strings.Replace(str, "{SSID}", name, 2)
+
+	file.Write([]byte(str))
+	return nil
+}
+
+func connectToNetwork(name string) bool {
 	// netsh wlan connect name=sideswipe
+	str := fmt.Sprintf("name=%s", name)
+	out, err := exec.Command("netsh", "wlan", "connect", str).Output()
+	must(err)
+	fmt.Print(out)
 	return false
 }
 
 func deleteProfile(name string) error {
+	out, err := exec.Command(`netsh`, `wlan`, `delete`, `profile`, name).Output()
+	must(err)
+	fmt.Print(string(out))
 	return nil
 }
 
@@ -34,7 +57,7 @@ func addProfile(name string) error {
 
 	out, err := exec.Command(`netsh`, `wlan`, `add`, `profile`, file, `interface="WI-FI"`, `user=current`).Output()
 	must(err)
-	fmt.Println(string(out))
+	fmt.Print(string(out))
 	return nil
 }
 
