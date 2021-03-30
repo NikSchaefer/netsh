166f5192f1a365c0ce322a1de1c2dbc161563f7c
NikSchaefer <59715513+NikSchaefer@users.noreply.github.com>
2021-03-30 13:01:56 -0500

    Baseline

--- main.go
+++ main.go
@@ -1,20 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os/exec"
+)
 
 func main() {
 	fmt.Println("HELLO WORLD")
 
-	const name = "sideswipe"
-	const password = ""
+	name := "sideswipe"
+	password := ""
 
 	passed := loginToNetwork(name, password)
 	fmt.Println(passed)
 }
 
-
 func loginToNetwork(name, password string) bool {
-	
-	
+	// netsh wlan connect name=sideswipe
 	return false
+}
+
+func deleteProfile(name string) error {
+	return nil
+}
+
+func addProfile(name string) error {
+	exec.Command(`netsh wlan add profile filename="C:\Users\schae\Desktop\Wi-Fi-sideswipe.xml" interface="WI-FI" user=current`)
+	return nil
 }
